48b6ad08bd684e2deb68ebcfdd5ead53adf277df
Seyed Ali Ghasemi <info@gha3mi.com>
2023-12-30 16:43:50 +0100

    Bug fix

--- example/example4.f90
+++ example/example4.f90
@@ -11,7 +11,7 @@ program example4
     do nl = 1, nloops
         call sleep(1) ! Perform operations ntimes
     end do
-   call t%ctimer_stop(nloops = n, message = 'CPU time:') ! nloops and message are optional
+   call t%ctimer_stop(nloops = nloops, message = 'CPU time:') ! nloops and message are optional
    call t%ctimer_write('example/example4_ctimes') ! Optionally, write the elapsed time to a file
 
 end program example4
