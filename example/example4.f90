329d7eba005531a309ef28fe07f38674d68726a2
Seyed Ali Ghasemi <info@gha3mi.com>
2023-12-30 16:40:51 +0100

    Update exmaples

--- example/example4.f90
+++ example/example4.f90
@@ -5,13 +5,13 @@ program example4
    implicit none
 
    type(timer) :: t
-   integer :: i, n=3
+   integer :: nl, nloops=3
 
    call t%ctimer_start()
-    do i = 1,n
+    do nl = 1, nloops
         call sleep(1) ! Perform operations ntimes
     end do
-   call t%ctimer_stop(nloops = n, message = 'CPU time:')
+   call t%ctimer_stop(nloops = n, message = 'CPU time:') ! nloops and message are optional
    call t%ctimer_write('example/example4_ctimes') ! Optionally, write the elapsed time to a file
 
 end program example4
